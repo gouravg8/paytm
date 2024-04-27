66b78503b348c0a06a58ceb24a78566ab50f32f0
Gourav soni <gouravsoni8476@gmail.com>
2024-04-27 21:49:05 +0530

    sending signin req to backend

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -6,22 +6,40 @@ import {
   Input,
   Subheading,
 } from "../components";
+import axios from "axios";
 
 const Signin = () => {
-  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  let domain = "http://localhost:3000/api/v1/user";
+
+  const handleSubmitSignin = async (e) => {
+    e.preventDefault();
+    let signinRes = await axios.post(
+      domain + "/signin",
+      {
+        username,
+        password,
+      },
+    );
+    console.log(signinRes);
+  };
   return (
     <div className="mx-auto w-2/3 md:w-1/2 lg:w-1/3 my-8">
       <Heading text={"Sign In"} key={"signinheading"} />
       <Subheading subheading={"Enter your credential to access your accont"} />
-      <div className="flex flex-col gap-4 my-3">
+      <form
+        method="post"
+        onSubmit={handleSubmitSignin}
+        className="flex flex-col gap-4 my-3"
+      >
         <Input
-          label={"Email"}
-          placeholder={"johnduo@example.com"}
-          value={email}
-          setValue={setEmail}
-          type={"email"}
-          key={"signinmail"}
+          label={"Username"}
+          placeholder={"Johndoe"}
+          value={username}
+          setValue={setUsername}
+          type={"text"}
+          key={"signinusername"}
         />
         <Input
           label={"Password"}
@@ -38,7 +56,7 @@ const Signin = () => {
           to={"/signup"}
           key={"signinwarning"}
         />
-      </div>
+      </form>
     </div>
   );
 };
